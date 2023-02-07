14a0439726fc6e1e8971c358bc51bc682b28d40f
MalfunctionCode <marvinjanderson@outlook.com>
2023-02-07 11:53:49 +0100

    fixed math plugin (possibly)

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ Reveal.initialize({
     hash: true,
 
     // Learn about plugins: https://revealjs.com/plugins/
-    plugins: [ RevealMarkdown, RevealHighlight, RevealNotes, RevealDrawer ]
+    plugins: [ RevealMarkdown, RevealHighlight, RevealNotes, RevealDrawer, RevealMath, RevealMath.KaTeX  ]
 });
